a06e9acf8b4ecaad3a27a8541506388555d66f8c
Braedy Kuzma <braedy@ualberta.ca>
2018-06-20 16:08:48 -0600

    Fix build bug.
    Also:
      - Add dummy target for project dependency.
      - Formatting updates.
      - Reorganise code to match the sister get_antlr_manual.

--- cmake/get_antlr.cmake
+++ cmake/get_antlr.cmake
@@ -5,36 +5,38 @@
 # CMake module that finds a previously installed copy of the ANTLR C++ runtime required when linking
 # a generated lexer/parser. Creates the variable ANTLR_INCLUDE_DIRS to add to your target's
 # include directories, adds the antlr library path to the project, allows your target to link
-# against antlr4-runtime, and creates ANTLR_JAR for generating grammars.
+# against antlr4-runtime, creates ANTLR_JAR for generating grammars, and creates an antlr target
+# that you can add via add_dependencies to make sure everything has happened already.
 
 # Get the environment variable that tells us where the manual install was.
 if (NOT DEFINED ENV{ANTLR_INS})
-  message(FATAL_ERROR "Did you forget to install ANTLR? The ANTLR_INS environment variable was not "
-                       "set.")
+  message(FATAL_ERROR "Did you forget to install ANTLR? The ANTLR_INS environment variable was "
+                      "not set.")
 endif()
-set(_ANTLR_DIR "$ENV{ANTLR_INS}")
+file(TO_CMAKE_PATH "$ENV{ANTLR_INS}" _ANTLR_DIR)
 
-# Set the directory for tools
+# Set the directory for binaries.
 file(TO_CMAKE_PATH "${_ANTLR_DIR}/bin" BIN_DIR) # Join dir.
 set(BIN_DIR ${BIN_DIR} CACHE PATH "ANTLR jar directory.") # Set for internal use.
-file(TO_NATIVE_PATH "${BIN_DIR}" BIN_DIR_NATIVE) # Transform for display.
 
 # Download ANTLR executable, saves us from ensuring people have java build tools (e.g. Maven)...
 file(TO_CMAKE_PATH "${BIN_DIR}/antlr-4.7.1-complete.jar" ANTLR_JAR)
 if (NOT EXISTS "${ANTLR_JAR}")
+  message(STATUS "Downloading ANTLR generator...")
   file(
     DOWNLOAD
     http://www.antlr.org/download/antlr-4.7.1-complete.jar
     "${ANTLR_JAR}"
     SHOW_PROGRESS
   )
- message(STATUS "Downloaded ANTLR jar destination: ${BIN_DIR_NATIVE}")
+  file(TO_NATIVE_PATH "${BIN_DIR}" BIN_DIR_NATIVE) # Transform for display.
+  message(STATUS "Downloaded ANTLR jar destination: ${BIN_DIR_NATIVE}")
 endif()
 
 # Check that the base include path exists.
-if (NOT EXISTS "${_ANTLR_DIR}/include/antlr4-runtime/${src_path}")
-    message(FATAL_ERROR "Missing ANTLR include dir: ${_ANTLR_DIR}/include/antlr4-runtime/${src_path}")
-  endif()
+if (NOT EXISTS "${_ANTLR_DIR}/include/antlr4-runtime/")
+  message(FATAL_ERROR "Missing ANTLR include dir: ${_ANTLR_DIR}/include/antlr4-runtime/")
+endif()
 
 # Create a list of include dirs with the base directory.
 list(APPEND ANTLR_INCLUDE_DIRS "${_ANTLR_DIR}/include/antlr4-runtime")
@@ -43,7 +45,8 @@ list(APPEND ANTLR_INCLUDE_DIRS "${_ANTLR_DIR}/include/antlr4-runtime")
 foreach(src_path misc atn dfa tree support)
   # Check that each of the directories exists, too many files to check individually.
   if (NOT EXISTS "${_ANTLR_DIR}/include/antlr4-runtime/${src_path}")
-    message(FATAL_ERROR "Missing ANTLR include dir: ${_ANTLR_DIR}/include/antlr4-runtime/${src_path}")
+    message(FATAL_ERROR "Missing ANTLR include dir: "
+                        "${_ANTLR_DIR}/include/antlr4-runtime/${src_path}")
   endif()
 
   # Add the directory to the includes list.
@@ -65,3 +68,7 @@ endforeach()
 # Create libs path and then add it to the linker paths.
 set(_ANTLR_LIB_DIRS "${_ANTLR_DIR}/lib")
 link_directories("${_ANTLR_LIB_DIRS}")
+
+# Add dummy target. There's nothing to be built because we're just importing antlr and exporting
+# values.
+add_custom_target(antlr)
